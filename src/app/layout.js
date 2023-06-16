410f433233e5d26283720fc633016deb55f87051
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-06-16 10:55:20 +0530

    hydration solver

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-"use client"
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,6 +13,11 @@ export default function RootLayout({ children }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
       </head>
       <body>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"
+      />
         {children}
       </body>
     </html>
